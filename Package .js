a10c20a8488abf61083fefc54584e889aaebd63e
Thanujqdilsan <thanujadilsan2011@gmail.com>
2025-07-22 11:04:35 +0530

    ↑THANUVA-MD↔

---
+++ Package .js
@@ -0,0 +1,19 @@
+{
+  "name": "thanuva-md",
+  "version": "1.0.0",
+  "description": "Speed optimized MD WhatsApp bot with Multi‑Device support",
+  "main": "bot.js",
+  "scripts": {
+    "start": "node bot.js",
+    "dev": "nodemon bot.js"
+  },
+  "keywords": ["whatsapp","baileys","md","multi-device","bot"],
+  "author": "Thanuja",
+  "license": "MIT",
+  "dependencies": {
+    "@adiwajshing/baileys": "^5.0.0",
+    "qrcode": "^1.5.0",
+    "express": "^4.18.2",
+    "nodemon": "^2.0.22"
+  }
+    }
