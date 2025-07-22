7cd7d25f7a2cb824b7088dba6b97dbb9ff1c3f42
Thanujqdilsan <thanujadilsan2011@gmail.com>
2025-07-22 11:07:18 +0530

    Create config .js

---
+++ config .js
@@ -0,0 +1,8 @@
+module.exports = {
+  ownerName: "Thanuja",
+  ownerNumber: "94702509233@s.whatsapp.net",
+  botName: "THANUVA‑MD",
+  sessionPath: "./auth_info_baileys/session.json",
+  qrPath: "./pair‑web/public/qr.png",
+  port: process.env.PORT || 3000
+};
